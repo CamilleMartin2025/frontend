e45944c5c38e26486759cb22f38d4c6b16086ad8
cmartin2025 <camille.martin2025@campus-eni.fr>
2026-05-12 14:40:30 +0200

    - ajout route pour espace admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { BookDetailComponent } from './pages/book-detail/book-detail.component';
 import { MySpaceComponent } from './pages/my-space/my-space-user/my-space.component';
 import { CatalogueComponent } from './pages/catalogue/catalogue.component';
+import { MySpaceAdminComponent } from './pages/my-space/my-space-admin/my-space-admin.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -11,5 +12,6 @@ export const routes: Routes = [
   { path: 'catalogue', component: CatalogueComponent },
   { path: 'book/:id', component: BookDetailComponent },
   { path: 'mon-espace', component: MySpaceComponent },
+  { path: 'mon-espace/admin', component: MySpaceAdminComponent },
   { path: '**', redirectTo: '' },
 ];
