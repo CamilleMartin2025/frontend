59bb64bbd2c5373c1ed3869445558ff228ad8905
cmartin2025 <camille.martin2025@campus-eni.fr>
2026-05-12 11:50:02 +0200

    -

---
+++ src/app/services/authentification.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models/book.model';
+
+// TODO : link with Utilisateur BDD
+@Injectable({ providedIn: 'root' })
+export class AuthentificationService {
+
+}
